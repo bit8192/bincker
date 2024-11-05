4a21a2c87f3c495b5e43ea87281f3fc3af030636
bincker <1229732681@qq.com>
2024-11-06 01:20:39 +0800

    blog同步脚本

---
+++ sync-blog.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -ex
+
+METHOD="$1"
+SERVER_HOST="$2"
+LOCAL_BLOG_PATH="blog"
+REMOTE_BLOG_PATH="/opt/bincker/blog"
+
+function usage() {
+    echo "$0 <pull|push> <SERVER_HOST>";
+    exit 1;
+}
+
+if [ "$METHOD" != "pull" ] && [ "$METHOD" != "push" ]; then
+    usage;
+fi
+
+if [ -z "$SERVER_HOST" ]; then
+    usage;
+fi
+
+function pull() {
+  rsync -chavzP --progress "$SERVER_HOST:$REMOTE_BLOG_PATH/" $LOCAL_BLOG_PATH;
+}
+
+function push() {
+  rsync -chavzP --progress blog/ "$SERVER_HOST:$REMOTE_BLOG_PATH"
+}
+
+$METHOD
