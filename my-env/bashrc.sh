1793ddff028639a9ee25ae72857ab9ef16dbd3f2
bincker <1229732681@qq.com>
2025-05-04 09:20:52 +0800

    修复proxy脚本在update脚本之后问题

--- my-env/bashrc.sh
+++ my-env/bashrc.sh
@@ -4,10 +4,10 @@ else
   MY_ENV="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 fi
 export MY_ENV
+source "$MY_ENV/proxy.sh"
 source "$MY_ENV/update.sh"
 source "$MY_ENV/adb.sh"
 source "$MY_ENV/deck.sh"
-source "$MY_ENV/proxy.sh"
 
 source "$MY_ENV/security/adb.sh"
 
