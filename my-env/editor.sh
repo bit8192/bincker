af174cefea3ed5aa678ae2fafc2c64b777a56361
bincker <1229732681@qq.com>
2025-11-29 23:17:49 +0800

    修复重复安装vim-plug的问题

--- my-env/editor.sh
+++ my-env/editor.sh
@@ -3,7 +3,7 @@ if ! command -v vim &>/dev/null; then return; fi
 export EDITOR=vim
 export VISUAL=vim
 
-if command -v pacman &>/dev/null && command -v yay &>/dev/null && ! pacman -Qd | grep -q vim-plug; then
+if command -v pacman &>/dev/null && command -v yay &>/dev/null && ! pacman -Qe | grep -q vim-plug; then
   proxy-set
   echo "not found vim-plug, will install..."
   yay -Sy vim-plug
